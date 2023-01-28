02a0200101c2cc954d85754511f721cdd4294bff
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-01-28 15:57:56 -0600

    Create code that will change color of boxes the mass passes over

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ for (let i = 0; i < 256; i++) {
     bodyContainer.appendChild(newBox);
 }
 
+let boxes = document.querySelectorAll('.grid-box');
+
+for (const box of boxes) {
+    box.addEventListener('mouseover', (e) => {
+        box.style.backgroundColor = 'red';
+    });
+}
+
 function createBox() {
     let box = document.createElement('div');
     box.classList.add('grid-box');
